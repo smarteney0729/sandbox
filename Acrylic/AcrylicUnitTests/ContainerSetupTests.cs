eb005909679ac2bb7b17a0b503f24a004e75040f
Steve Marteney <steve.marteney@gmail.com>
2017-08-16 17:00:25 -0400

    Issue #1 The container must allow you to register types

--- Acrylic/AcrylicUnitTests/ContainerSetupTests.cs
+++ Acrylic/AcrylicUnitTests/ContainerSetupTests.cs
@@ -7,12 +7,39 @@ namespace AcrylicUnitTests
     public class ContainerSetupTests
     {
         [Fact]
+        public void When_container_is_empty_IsRegistered_returns_false()
+        {
+            var container = new AcrylicContainer();
+            Assert.False(container.IsRegistered(typeof(ITextStream)));
+        }
+
+        [Fact]
+        public void When_type_not_registerd_IsRegistered_returns_false()
+        {
+            var container = new AcrylicContainer();
+
+            container.Register<BaseClass, ConcreteClass>();
+            container.Register<ITextStream, TextStream>();
+
+            Assert.False(container.IsRegistered(typeof(ICalculator)));
+        }
+
+        [Fact]
         public void Container_allows_type_registration()
         {
             var container = new AcrylicContainer();
             container.Register<ITextStream, TextStream>();
 
             Assert.True(container.IsRegistered(typeof(ITextStream)));
+        }
+
+        [Fact]
+        public void Container_allows_type_registration_of_abstract_class()
+        {
+            var container = new AcrylicContainer();
+            container.Register<BaseClass, ConcreteClass>();
+
+            Assert.True(container.IsRegistered(typeof(BaseClass)));
         }
 
         [Fact]
@@ -25,8 +52,11 @@ namespace AcrylicUnitTests
             Assert.True(container.IsRegistered(typeof(ITextStream)));
             //need to verify the container indeed holds registration for
             //AsciiStream as the implementation of ITextStream 
-            //Need to implement Resolve first.
+            //Need to implement Resolve first, or a Spy that can
+            //look at the internals of the container.
         }
 
     }
+
+   
 }
---
+++ Acrylic/AcrylicUnitTests/TestClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcrylicUnitTests
+{
+    internal interface ICalculator
+    {
+        T Add<T>(T a, T b) where T : struct;
+    }
+
+
+
+
+    public interface ITextStream
+    {
+    }
+
+    public class TextStream : ITextStream { }
+    public class AsciiStream : ITextStream { }
+
+    public abstract class BaseClass
+    {
+        abstract public void Method();
+
+        protected virtual int MethodA(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class ConcreteClass : BaseClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Implemented Method");
+        }
+    }
+}
